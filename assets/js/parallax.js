fa0168eca2521b8927d42913252b6e610c92f8a1
Ludmila Sukhomudrenko <lydmilazoloto@gmail.com>
2017-08-06 15:41:43 +0300

    fix parallax

--- assets/js/parallax.js
+++ assets/js/parallax.js
@@ -1,42 +1,32 @@
 (function( $ ) {
     'use strict';
 
-    $.fn.myParallax = function(options) {
+    $.fn.myParallax = function() {
 
         return this.each(function() {
             var $this = $(this);
 
             $(window).on('scroll', function () {
-                parallaxAction(false);
+                parallaxAction();
             });
 
-            function parallaxAction(timeOut) {
+            function parallaxAction() {
                 var st = $(window).scrollTop() - $this.offset().top + $this.height();
-                if (timeOut) {
-                    var doit;
-                    clearTimeout(doit);
-                    doit = setTimeout(parallax, 100);
-                } else {
-                    parallax();
+
+                $this.css({
+                    'background-attachment': 'fixed'
+                });
+                if ($(window).width() > 767) {
+
+                    $this.find('div').css({
+                        'transform': 'translate(0px, ' + st / 3 + 'px)'
+                    })
                 }
 
-
-                function parallax() {
-                    $this.css({
-                        'background-attachment': 'fixed'
-                    });
-                    if ($(window).width() > 767) {
-
-                        $this.find('div').css({
-                            'transform': 'translate(0px, ' + st / 3 + 'px)'
-                        })
-                    }
-
-                    else {
-                        $this.find('div').css({
-                            'transform': 'none'
-                        })
-                    }
+                else {
+                    $this.find('div').css({
+                        'transform': 'none'
+                    })
                 }
             }
         });
